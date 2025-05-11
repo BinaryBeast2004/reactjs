ee0296141ef5cb9810a13b0fbbafbb671725f1ab
antran9128 <an482194@gmail.com>
2025-05-11 23:05:35 +0700

    video 16

---
+++ src/component/MyComponent.js
@@ -0,0 +1,12 @@
+// Class component
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        //JSX
+        return <div>My First Component</div>;
+    }
+}
+
+export default MyComponent;
+// Function component
